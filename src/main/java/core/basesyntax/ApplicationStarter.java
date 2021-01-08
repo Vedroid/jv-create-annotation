ac2fdbe56b9adac14675bd27f8c76c9d7f189708
Vadim <Vedroidua@gmail.com>
2021-01-08 15:37:37 +0200

    Added Inject in ConsoleHandler

--- src/main/java/core/basesyntax/ApplicationStarter.java
+++ src/main/java/core/basesyntax/ApplicationStarter.java
@@ -2,10 +2,13 @@ package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
 import core.basesyntax.db.Storage;
+import core.basesyntax.lib.Injector;
+import java.lang.reflect.InvocationTargetException;
 
 public class ApplicationStarter {
-    public static void main(String[] args) {
-        ConsoleHandler consoleHandler = new ConsoleHandler();
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException {
+        ConsoleHandler consoleHandler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         consoleHandler.handle();
 
         Storage.bets.forEach(bet ->
